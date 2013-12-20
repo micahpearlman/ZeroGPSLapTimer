e0c8eb36b1bada0bd8463992b82fbcd06f649613
Micah Pearlman <micahpearlman@gmail.com>
2013-12-20 11:37:20 -0800

    first commit

---
+++ ZeroGPSLapTimer/src/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZeroGPSLapTimer
+//
+//  Created by Micah Pearlman on 12/20/13.
+//  Copyright (c) 2013 Micah Pearlman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZOAppDelegate class]));
+	}
+}
