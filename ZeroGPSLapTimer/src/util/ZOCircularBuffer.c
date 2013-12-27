6c6d374b4ee704a52b5b317b11b9f0c13049322d
Micah Pearlman <micahpearlman@gmail.com>
2013-12-26 22:53:10 -0800

    added circular buffer implementation

---
+++ ZeroGPSLapTimer/src/util/ZOCircularBuffer.c
@@ -0,0 +1,9 @@
+//
+//  ZOCircularBuffer.c
+//  ZeroGPSLapTimer
+//
+//  Created by Micah Pearlman on 12/26/13.
+//  Copyright (c) 2013 Micah Pearlman. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ ZeroGPSLapTimer/src/util/ZOCircularBuffer.h
@@ -0,0 +1,14 @@
+//
+//  ZOCircularBuffer.h
+//  ZeroGPSLapTimer
+//
+//  Created by Micah Pearlman on 12/26/13.
+//  Copyright (c) 2013 Micah Pearlman. All rights reserved.
+//
+
+#ifndef ZeroGPSLapTimer_ZOCircularBuffer_h
+#define ZeroGPSLapTimer_ZOCircularBuffer_h
+
+
+
+#endif
