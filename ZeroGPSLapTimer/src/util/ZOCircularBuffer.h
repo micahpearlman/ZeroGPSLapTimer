d40007a5d668b07a19db9980497b90b8c984e9d6
Micah Pearlman <micahpearlman@gmail.com>
2013-12-26 22:54:41 -0800

    implementation of circular buffer

--- ZeroGPSLapTimer/src/util/ZOCircularBuffer.h
+++ ZeroGPSLapTimer/src/util/ZOCircularBuffer.h
@@ -8,7 +8,15 @@
 
 #ifndef ZeroGPSLapTimer_ZOCircularBuffer_h
 #define ZeroGPSLapTimer_ZOCircularBuffer_h
+#include <stdlib.h>
+#include <stdint.h>
 
+typedef void* ZOCircularBuffer;
 
+extern ZOCircularBuffer zoCircularBufferInit( uint32_t sz );
+extern void zoCircularBufferDestroy( ZOCircularBuffer cb );
+extern void zoCircularBufferWrite( ZOCircularBuffer cb, const uint8_t* data, const uint32_t length );
+extern void zoCircularBufferRead( ZOCircularBuffer cb, uint8_t* buffer, const uint32_t length );
+extern uint32_t zoCircularBufferGetCount( ZOCircularBuffer cb );
 
 #endif
